78b15431fbab7ad45fcc4fe794e0d19ea77b7066
Swapnil Nigam <swapnilnigam2410@gmail.com>
2024-09-22 21:25:20 +0530

    API completed for create Rating, get All Rating, get rating using userid amd hotelid

--- src/main/java/com/lcwd/rating/RatingServiceApplication.java
+++ src/main/java/com/lcwd/rating/RatingServiceApplication.java
@@ -2,8 +2,10 @@ package com.lcwd.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lcwd.rating")
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lcwd/rating/controller/RatingController.java
@@ -0,0 +1,45 @@
+package com.lcwd.rating.controller;
+
+import com.lcwd.rating.entities.Rating;
+import com.lcwd.rating.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+
+    //create Rating
+    @PostMapping
+    public ResponseEntity<Rating> create (@RequestBody Rating rating) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
+    }
+
+    //get all
+    @GetMapping("/getallratings")
+    public ResponseEntity<List<Rating>> getRatings() {
+        return ResponseEntity.ok(ratingService.getRatings());
+    }
+
+    //get all
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String userId) {
+        return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
+    }
+
+    //get all
+    @GetMapping("/hotels/{hotelId}")
+    public ResponseEntity<List<Rating>> getRatingsByHotelId(@PathVariable String hotelId) {
+        return ResponseEntity.ok(ratingService.getRatingByHotelId(hotelId));
+    }
+
+
+
+}
